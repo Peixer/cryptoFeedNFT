5f9cfc74b8db0e6c039996d73b5a7af5cd13873d
Glaicon <gjpeixer@gmail.com>
2023-09-28 00:02:09 -0400

    update next.config

--- next.config.js
+++ next.config.js
@@ -4,6 +4,7 @@ module.exports = {
       "res.cloudinary.com",
       "scontent.cdninstagram.com",
       "cdninstagram.com",
+      "pbxt.replicate.delivery"
     ],
     remotePatterns: [
       {
