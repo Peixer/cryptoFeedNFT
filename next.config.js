aa7154fdbe4aa45481b77ce3dcb8acd87841d45d
Glaicon <gjpeixer@gmail.com>
2023-09-27 23:34:18 -0400

    update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,6 +1,10 @@
 module.exports = {
   images: {
-    formats: ['image/avif', 'image/webp'],
-    domains: ['res.cloudinary.com','scontent.cdninstagram.com'],
+    formats: ["image/avif", "image/webp"],
+    domains: [
+      "res.cloudinary.com",
+      "scontent.cdninstagram.com",
+      "https://scontent-iad3-1.cdninstagram.com/",
+    ],
   },
-}
+};
