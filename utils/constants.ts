82c580fc4d3311799251c5a79a0353c1b22c9b60
Glaicon <gjpeixer@gmail.com>
2023-10-03 13:13:21 -0400

    WIP - update api medias

--- utils/constants.ts
+++ utils/constants.ts
@@ -9,30 +9,6 @@ export const mockJson = `
                             "id": "3197856229610855857",
                             "display_url": "https://scontent-gru1-2.cdninstagram.com/v/t51.2885-15/381355408_1459757914821951_9034972423939675131_n.webp?stp=dst-jpg_e35&_nc_ht=scontent-gru1-2.cdninstagram.com&_nc_cat=103&_nc_ohc=v-kuquqKN3kAX9f0RQn&edm=ABmJApABAAAA&ccb=7-5&ig_cache_key=MzE5Nzg1NjIyOTYxMDg1NTg1Nw%3D%3D.2-ccb7-5&oh=00_AfBQua5s1-oID1-vt_yPAsN4mxJI0oxEugDyvzluo3IDnQ&oe=651FAF1C&_nc_sid=b41fef"
                         }
-                    },
-                    {
-                        "node": {
-                            "id": "3196742744322867482",
-                            "display_url": "https://scontent-gru1-2.cdninstagram.com/v/t51.2885-15/381231790_181870148274108_6713860512703636514_n.webp?stp=dst-jpg_e35&_nc_ht=scontent-gru1-2.cdninstagram.com&_nc_cat=100&_nc_ohc=9bXBxuLc2noAX8kNeZY&edm=ABmJApABAAAA&ccb=7-5&ig_cache_key=MzE5Njc0Mjc0NDMyMjg2NzQ4Mg%3D%3D.2-ccb7-5&oh=00_AfCju9fCsv9PzlbyJoW0n4ha5mXE7poKsdfNeKT6yuTfNw&oe=651FDF72&_nc_sid=b41fef"
-                        }
-                    },
-                    {
-                        "node": {
-                            "id": "3157841445392834864",
-                            "display_url": "https://scontent-gru2-1.cdninstagram.com/v/t51.2885-15/363833563_831402065015925_4392545236883663913_n.webp?stp=dst-jpg_e35&_nc_ht=scontent-gru2-1.cdninstagram.com&_nc_cat=109&_nc_ohc=BwosKKNgnqMAX896CiB&edm=ABmJApABAAAA&ccb=7-5&ig_cache_key=MzE1Nzg0MTQ0NTM5MjgzNDg2NA%3D%3D.2-ccb7-5&oh=00_AfBQD8O8nAjSHRCPEn35zTuYJxi7_C4ZxgGUBX-nrJY8EA&oe=651F7FA7&_nc_sid=b41fef"
-                        }
-                    },
-                    {
-                        "node": {
-                            "id": "3151906079947731388",
-                            "display_url": "https://scontent-gru2-2.cdninstagram.com/v/t51.2885-15/361957326_778573680671126_8451589828582417064_n.webp?stp=dst-jpg_e35&_nc_ht=scontent-gru2-2.cdninstagram.com&_nc_cat=102&_nc_ohc=ZsaR0g_6i98AX-3CiR6&edm=ABmJApABAAAA&ccb=7-5&ig_cache_key=MzE1MTkwNjA3OTk0NzczMTM4OA%3D%3D.2-ccb7-5&oh=00_AfCCdT0RcuHtRgcA54DdM-DGg2xS_CGeYQbrcLdpNXCBGQ&oe=651FD9F2&_nc_sid=b41fef"
-                        }
-                    },
-                    {
-                        "node": {
-                            "id": "2974271896690700130",
-                            "display_url": "https://scontent-gru1-2.cdninstagram.com/v/t51.2885-15/315992322_463476169188812_6962265424525393181_n.webp?stp=dst-jpg_e35&_nc_ht=scontent-gru1-2.cdninstagram.com&_nc_cat=108&_nc_ohc=BHfAqIU9IN4AX-Y9bLo&edm=ABmJApABAAAA&ccb=7-5&ig_cache_key=Mjk3NDI3MTg5NjY5MDcwMDEzMA%3D%3D.2-ccb7-5&oh=00_AfDbT7QUquImjkQ624M9atkT7B1Zb-k0PCLu8kUd_I5Xxw&oe=651F514D&_nc_sid=b41fef"
-                        }
                     }
                 ]
             }
