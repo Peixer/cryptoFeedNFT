2153569d0ba60e8d3fd68f4e6dbbfa59adb5c16c
Glaicon <gjpeixer@gmail.com>
2023-10-03 12:34:12 -0400

    add mock instagram response

--- pages/api/medias/index.tsx
+++ pages/api/medias/index.tsx
@@ -2,22 +2,28 @@ import type { NextApiResponse } from "next";
 import axios from "axios";
 import { authOptions } from "../auth/[...nextauth]";
 import { getServerSession } from "next-auth";
+import { mockJson } from "../../../utils/constants";
 
 const handler = async (req: any, res: NextApiResponse) => {
   const session = await getServerSession(req, res, authOptions);
   if (session) {
-    const medias = await axios.get(
-      `https://api.hikerapi.com/a2/user?username=${session.user.name}`,
-      {
-        headers: {
-          accept: "application/json",
-          "x-access-key": process.env.HIKER_API_CLIENTSECRET,
-        },
-      }
-    );
+    let mediasResponse = {} as any;
+    if (process.env.NODE_ENV === "development") {
+      mediasResponse = { data: JSON.parse(mockJson) };
+    } else {
+      mediasResponse = await axios.get(
+        `https://api.hikerapi.com/a2/user?username=${session.user.name}`,
+        {
+          headers: {
+            accept: "application/json",
+            "x-access-key": process.env.HIKER_API_CLIENTSECRET,
+          },
+        }
+      );
+    }
 
     const mediaUrls =
-      medias.data.graphql.user.edge_owner_to_timeline_media.edges.map(
+      mediasResponse.data.graphql.user.edge_owner_to_timeline_media.edges.map(
         (media) => {
           return media.node.display_url;
         }
