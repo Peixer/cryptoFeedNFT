45af31b5c2b6598fd2c87f3466b707b43f52524c
Glaicon <gjpeixer@gmail.com>
2023-10-03 13:46:16 -0400

    rollback mock json changes

--- pages/api/medias/index.tsx
+++ pages/api/medias/index.tsx
@@ -2,27 +2,7 @@ import type { NextApiResponse } from "next";
 import axios from "axios";
 import { authOptions } from "../auth/[...nextauth]";
 import { getServerSession } from "next-auth";
-// import { mockJson } from "../../../utils/constants";
-
-export const mockJson = `
-{
-  "data":{
-    "graphql": {
-        "user": {
-            "edge_owner_to_timeline_media": {
-                "edges": [
-                    {
-                        "node": {
-                            "id": "3197856229610855857",
-                            "display_url": "https://scontent-gru1-2.cdninstagram.com/v/t51.2885-15/381355408_1459757914821951_9034972423939675131_n.webp?stp=dst-jpg_e35&_nc_ht=scontent-gru1-2.cdninstagram.com&_nc_cat=103&_nc_ohc=v-kuquqKN3kAX9f0RQn&edm=ABmJApABAAAA&ccb=7-5&ig_cache_key=MzE5Nzg1NjIyOTYxMDg1NTg1Nw%3D%3D.2-ccb7-5&oh=00_AfBQua5s1-oID1-vt_yPAsN4mxJI0oxEugDyvzluo3IDnQ&oe=651FAF1C&_nc_sid=b41fef"
-                        }
-                    }
-                ]
-            }
-        }
-    }
-  }
-}`;
+import { mockJson } from "../../../utils/constants";
 
 const handler = async (req: any, res: NextApiResponse) => {
   const session = await getServerSession(req, res, authOptions);
