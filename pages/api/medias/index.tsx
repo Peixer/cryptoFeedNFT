70763928daf5661535e660fa25c03169b4252eb8
Glaicon <gjpeixer@gmail.com>
2023-10-03 13:56:28 -0400

    update medias api

--- pages/api/medias/index.tsx
+++ pages/api/medias/index.tsx
@@ -10,7 +10,7 @@ const handler = async (req: any, res: NextApiResponse) => {
     let mediasResponse = {} as any;
     let useMockJson = true;
     if (useMockJson) {
-      mediasResponse = JSON.parse(mockJson);
+      res.status(200).json({ medias: mockJson });
     } else {
       mediasResponse = await axios.get(
         `https://api.hikerapi.com/a2/user?username=${session.user.name}`,
@@ -21,15 +21,15 @@ const handler = async (req: any, res: NextApiResponse) => {
           },
         }
       );
-    }
 
-    const mediaUrls =
-      mediasResponse.data.graphql.user.edge_owner_to_timeline_media.edges.map(
-        (media) => {
-          return media.node.display_url;
-        }
-      );
-    res.status(200).json({ medias: mediaUrls });
+      const mediaUrls =
+        mediasResponse.data.graphql.user.edge_owner_to_timeline_media.edges.map(
+          (media) => {
+            return media.node.display_url;
+          }
+        );
+      res.status(200).json({ medias: mediaUrls });
+    }
   }
   res.status(200).json({ medias: [] });
 };
