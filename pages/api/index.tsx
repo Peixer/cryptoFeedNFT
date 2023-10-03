fff845b90d2189bc360d56fe3a6a74d6215adcdd
Glaicon <gjpeixer@gmail.com>
2023-10-03 14:31:05 -0400

    add try/catch in api index

--- pages/api/index.tsx
+++ pages/api/index.tsx
@@ -10,22 +10,24 @@ export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse<ResponseData>
 ) {
-  const replicate = new Replicate({
-    auth: process.env.REPLICATE_API_TOKEN,
-  });
+  try {
+    const replicate = new Replicate({
+      auth: process.env.REPLICATE_API_TOKEN,
+    });
 
-  const output = await replicate.run(
-    "stability-ai/sdxl:af1a68a271597604546c09c64aabcd7782c114a63539a4a8d14d1eeda5630c33",
-    {
-      input: {
-        prompt: "cartoon style with nostalgic pixel art look",
-        image: req.body.media_url,
+    const output = await replicate.run(
+      "stability-ai/sdxl:af1a68a271597604546c09c64aabcd7782c114a63539a4a8d14d1eeda5630c33",
+      {
+        input: {
+          prompt: "cartoon style with nostalgic pixel art look",
+          image: req.body.media_url,
+        },
+      }
+    );
 
-      },
-    }
-  );
-
-  res
-    .status(200)
-    .json({ output: output[0] });
+    res.status(200).json({ output: output[0] });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ output: error });
+  }
 }
