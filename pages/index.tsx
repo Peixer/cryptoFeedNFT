6fb6a2574fdc4894ed52da274bd4a51b3b325bc9
Glaicon <gjpeixer@gmail.com>
2023-10-03 21:06:12 -0400

    update search bar field

--- pages/index.tsx
+++ pages/index.tsx
@@ -58,7 +58,7 @@ const Home: NextPage = () => {
                 placeholder="Enter Instagram username"
                 onChange={(e) => setUsername(e.target.value)}
                 id="email"
-                className="block w-full rounded-md border-0 px-3.5 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                className="block w-full rounded-md border-0 px-3.5 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 text-4xl sm:leading-6"
               />
             </form>
           </div>
