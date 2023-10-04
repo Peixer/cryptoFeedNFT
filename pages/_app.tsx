65410e7eb7a5b31e52e03a4cd8cf33ad8b277a9b
Vinicius Klöppel <56376130+viniciuskloppel@users.noreply.github.com>
2023-10-04 13:38:25 -0300

    feat: integrate AI image generation with mint feature

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,16 +1,19 @@
 import App from "next/app";
 import "../styles/index.css";
+import { Wallet } from "../components/Wallet";
 
 class MyApp extends App {
-  render() {
-    const {
-      Component,
-      pageProps: { ...pageProps },
-    } = this.props;
-    return (
-        <Component {...pageProps} />
-    );
-  }
+    render() {
+        const {
+            Component,
+            pageProps: { ...pageProps },
+        } = this.props;
+        return (
+            <Wallet>
+                <Component {...pageProps} />
+            </Wallet>
+        );
+    }
 }
 
 export default MyApp;
--- pages/index.tsx
+++ pages/index.tsx
@@ -3,128 +3,166 @@ import Head from "next/head";
 import Image from "next/image";
 import type { ImageProps } from "../utils/types";
 import Link from "next/link";
-import { useState } from "react";
+import {
+    FormEvent,
+    FormEventHandler,
+    useCallback,
+    useEffect,
+    useLayoutEffect,
+    useState,
+} from "react";
 import { signOut } from "next-auth/react";
 import axios from "axios";
+import { useRouter } from "next/router";
 
 const Home: NextPage = () => {
-  const [images, setImages] = useState<ImageProps[]>([]);
-  const [username, setUsername] = useState<string>("");
-  const [loading, setLoading] = useState<boolean>(false);
+    const router = useRouter();
+    const initialUsername = (router.query.username as string) ?? "";
 
-  async function getUserPhotos(e) {
-    e.preventDefault();
-    setLoading(true);
-    const mediaResponse = await axios.get(`/api/medias?username=${username}`);
+    const [images, setImages] = useState<ImageProps[]>([]);
+    const [loading, setLoading] = useState<boolean>(false);
+    const [username, setUsername] = useState<string>(initialUsername);
 
-    if (mediaResponse.data) {
-      setImages(
-        mediaResponse.data.medias
-          .filter((x) => x)
-          .map((item: any, index) => {
-            return {
-              id: index,
-              media_url: item,
-              media_type: "",
-              username: "",
-              timestamp: "",
-            } as ImageProps;
-          })
-      );
+    function handleSubmit(e: FormEvent<HTMLFormElement>) {
+        e.preventDefault();
+        router.push(`/?username=${username}`, undefined);
+        if (!username) {
+            setImages([]);
+            router.push("/");
+            return;
+        }
+        getUserPhotos();
     }
-    setLoading(false);
-  }
 
-  return (
-    <>
-      <Head>
-        <title>Crypto Feed NFT</title>
-        <meta
-          property="og:image"
-          content="https://www.cryptofeednft.com/logo.png"
-        />
-        <meta
-          name="twitter:image"
-          content="https://www.cryptofeednft.com/logo.png"
-        />
-      </Head>
-      <main className="mx-auto max-w-[1960px] p-4">
-        <div className="sm:col-span-2">
-          <div className="mb-5 mt-2.5">
-            <form onSubmit={getUserPhotos}>
-              <input
-                type="text"
-                name="email"
-                placeholder="Enter Instagram username"
-                onChange={(e) => setUsername(e.target.value)}
-                id="email"
-                className="block w-full rounded-md border-0 px-3.5 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 text-4xl sm:leading-6"
-              />
-            </form>
-          </div>
-        </div>
-        {loading && (
-          <div className="flex items-center justify-center text-white">
-            <svg
-              className="-ml-1 mr-3 h-10 w-10 animate-spin text-white"
-              xmlns="http://www.w3.org/2000/svg"
-              fill="none"
-              viewBox="0 0 24 24"
-            >
-              <circle
-                className="opacity-25"
-                cx="12"
-                cy="12"
-                r="10"
-                stroke="currentColor"
-                strokeWidth="4"
-              ></circle>
-              <path
-                className="opacity-75"
-                fill="currentColor"
-                d="M4 12a8 8 0 018-8v2a6 6 0 00-6 6z"
-              ></path>
-            </svg>
-            <span className="text-white">Loading...</span>
-          </div>
-        )}
-        <div className="columns-1 gap-4 sm:columns-2 xl:columns-3 2xl:columns-4">
-          {images.map(({ id, media_url }) => (
-            <Link
-              key={id}
-              href={`/p/?media_url=${encodeURIComponent(media_url)}`}
-              shallow
-              className="after:content group relative mb-5 block w-full cursor-zoom-in after:pointer-events-none after:absolute after:inset-0 after:rounded-lg after:shadow-highlight"
-            >
-              <Image
-                alt="selected image"
-                className="transform rounded-lg brightness-90 transition will-change-auto group-hover:brightness-110"
-                style={{ transform: "translate3d(0, 0, 0)" }}
-                placeholder="blur"
-                id={id}
-                src={media_url}
-                blurDataURL={media_url}
-                width={720}
-                height={480}
-                sizes="(max-width: 640px) 100vw,
+    const getUserPhotos = useCallback(async () => {
+        setLoading(true);
+        const mediaResponse = await axios.get(
+            `/api/medias?username=${username}`
+        );
+
+        if (mediaResponse.data) {
+            setImages(
+                mediaResponse.data.medias
+                    .filter((x) => x)
+                    .map((item: any, index) => {
+                        return {
+                            id: index,
+                            media_url: item,
+                            media_type: "",
+                            username: "",
+                            timestamp: "",
+                        } as ImageProps;
+                    })
+            );
+        }
+        setLoading(false);
+    }, []);
+
+    useEffect(() => {
+        const queryUsername = router.query.username as string;
+        if (!queryUsername) return setUsername("");
+        setUsername(queryUsername);
+    }, [router.query.username]);
+
+    useEffect(() => {
+        if (!username) {
+            setImages([]);
+            return;
+        }
+        getUserPhotos();
+    }, [username]);
+
+    return (
+        <>
+            <Head>
+                <title>Crypto Feed NFT</title>
+                <meta
+                    property="og:image"
+                    content="https://www.cryptofeednft.com/logo.png"
+                />
+                <meta
+                    name="twitter:image"
+                    content="https://www.cryptofeednft.com/logo.png"
+                />
+            </Head>
+            <main className="mx-auto max-w-[1960px] p-4">
+                <div className="sm:col-span-2">
+                    <div className="mb-5 mt-2.5">
+                        <form onSubmit={handleSubmit}>
+                            <input
+                                type="text"
+                                name="email"
+                                placeholder="Enter Instagram username"
+                                onChange={(e) => setUsername(e.target.value)}
+                                value={username}
+                                id="email"
+                                className="block w-full rounded-md border-0 px-3.5 py-2 text-4xl text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:leading-6"
+                            />
+                        </form>
+                    </div>
+                </div>
+                {loading && (
+                    <div className="flex items-center justify-center text-white">
+                        <svg
+                            className="-ml-1 mr-3 h-10 w-10 animate-spin text-white"
+                            xmlns="http://www.w3.org/2000/svg"
+                            fill="none"
+                            viewBox="0 0 24 24"
+                        >
+                            <circle
+                                className="opacity-25"
+                                cx="12"
+                                cy="12"
+                                r="10"
+                                stroke="currentColor"
+                                strokeWidth="4"
+                            ></circle>
+                            <path
+                                className="opacity-75"
+                                fill="currentColor"
+                                d="M4 12a8 8 0 018-8v2a6 6 0 00-6 6z"
+                            ></path>
+                        </svg>
+                        <span className="text-white">Loading...</span>
+                    </div>
+                )}
+                <div className="columns-1 gap-4 sm:columns-2 xl:columns-3 2xl:columns-4">
+                    {images.map(({ id, media_url }) => (
+                        <Link
+                            key={id}
+                            href={`/p/?media_url=${encodeURIComponent(
+                                media_url
+                            )}`}
+                            shallow
+                            className="after:content group relative mb-5 block w-full cursor-zoom-in after:pointer-events-none after:absolute after:inset-0 after:rounded-lg after:shadow-highlight"
+                        >
+                            <Image
+                                alt="selected image"
+                                className="transform rounded-lg brightness-90 transition will-change-auto group-hover:brightness-110"
+                                style={{ transform: "translate3d(0, 0, 0)" }}
+                                id={id}
+                                src={media_url}
+                                width={720}
+                                height={480}
+                                sizes="(max-width: 640px) 100vw,
                   (max-width: 1280px) 50vw,
                   (max-width: 1536px) 33vw,
                   25vw"
-              />
-            </Link>
-          ))}
-        </div>
-      </main>
-    </>
-  );
+                            />
+                        </Link>
+                    ))}
+                </div>
+            </main>
+        </>
+    );
 };
 
 export default Home;
 
 export async function getStaticProps() {
-  return {
-    props: {
-      images: [],
-    },
-  };
+    return {
+        props: {
+            images: [],
+        },
+    };
 }
